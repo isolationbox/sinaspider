44cb00e22c7f40118badc007928e1e43a3e46773
橡皮擦 <yangjusheng1314@gmail.com>
2018-09-24 01:23:04 +0800

    开始抓取

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import siansql
 import requestmethod
 
-symbols = siansql.getSymbols(1,20,'all')
-data = requestmethod.getDetail(symbols)
-print(data)
+page = 1
+num = 20
+while True:
+    symbols = siansql.getSymbols(page, num, 'all')
+    if len(symbols) == 0 :
+        break
+    data = requestmethod.getDetail(symbols)
+    arr = []
+    for i,v in enumerate(data):
+        arr.append((symbols[i],v[0],v[3],v[6],v[7],v[2],v[1],v[4],v[5],v[8],v[9]))
+    # 代号，名称，最新价，买入，卖出，昨收，今开，最高，最低，成交量(股票数，一般要除100)，成交额(元，一般除万)
+    sql = 'insert into detail (symbol,name,price,buy,sell,settlement,open,high,low,volume,amount) value (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+    siansql.saveList(sql, arr)
+    if num != len(symbols):
+        break
+    page += 1
