b57391fb3e4108f34c7534c0d2e2ab3e1996517b
杨举圣 <yangjusheng@qq.com>
2018-10-04 12:23:46 +0800

    后端接口

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import request
+import sinasql
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello'
+@app.route('/api/getlastinfo', methods=['POST'])
+def getLastInfoByNode():
+    res = sinasql.getLastInfoByNode(request.form['node'],request.form['page'],request.form['num'])
+    return res
+
+@app.route('/api/gethistory', methods=['POST'])
+def getHistory():
+    print(request.method)
+    res = sinasql.getHistory(request.form['code'],request.form['num'])
+    return res
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- sinasql.py
+++ sinasql.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 import pymysql
 import sys
+import json
 
 def dbCon():
   con = pymysql.connect(host='localhost',user='sinaspider',password='123456', database='sinaspider')
@@ -23,7 +24,7 @@ def getSymbols(page, num = 20, node='all'):
   sql = 'select symbol from NodeList'
   if node != 'all':
     sql += ' where node=\'%s\''%node
-  sql += ' limit %d,%d'%((page - 1) * num + 1, num)
+  sql += ' limit %d,%d'%((page - 1) * num, num)
   try:
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -33,3 +34,47 @@ def getSymbols(page, num = 20, node='all'):
     print(sys.exc_info())
   con.close()
   return res
+
+def getLastInfoByNode(node='dpzs', page=1 , num=20):
+  print('%s,%s,%s'%(node,page,num))
+  page = int(page)
+  num = int(num)
+  con,cursor = dbCon()
+  try:
+    sql = 'select MAX(date) from stock_day_line left join NodeList on stock_day_line.code = NodeList.code where NodeList.node=\'%s\''%node
+    print(sql)
+    cursor.execute(sql)
+    date = cursor.fetchone()[0]
+    print(date)
+    sql = 'SELECT stock_day_line.* FROM stock_day_line LEFT JOIN NodeList ON stock_day_line.code = NodeList.code WHERE NodeList.node = \'%s\' and stock_day_line.date =\'%s\' limit %d,%d'%(node,date,(page-1)*num,num)
+    print(sql)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    if len(data) > 0:
+      sql = 'select count(*) from stock_day_line LEFT JOIN NodeList ON stock_day_line.code = NodeList.code where NodeList.node=\'%s\' and date = \'%s\''%(node,date)
+      cursor.execute(sql)
+      total = cursor.fetchone()[0]
+    else:
+      total = 0
+    res = { 'error': False, 'data': data,'total': total}
+  except:
+    print(sys.exc_info()[0])
+    res = { 'error': True, 'err_msg': '内部错误' }
+  con.close()
+  return json.dumps(res)
+
+def getHistory(code, num=30):
+  print('%s,%s'%(code,num))
+  num = int(num)
+  con,cursor = dbCon()
+  try:
+    sql = 'select * from stock_day_line where code=\'%s\' order by date limit %d'%(code,num)
+    print(sql)
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    res = { 'error': False, 'data': data }
+  except:
+    print(sys.exc_info()[1])
+    res = { 'error': True, 'err_msg': '内部错误' }
+  con.close()
+  return json.dumps(res)
